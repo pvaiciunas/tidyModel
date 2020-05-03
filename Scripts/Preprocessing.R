c97e3a9394ca68f83c93dcc8471707739f51a1d3
pvaiciunas <p.l.vaiciunas@gmail.com>
2020-05-03 14:32:52 -0400

    initial package load for preprocessing

---
+++ Scripts/Preprocessing.R
@@ -0,0 +1,9 @@
+# BAsed off of:
+# https://www.tidymodels.org/start/recipes/
+# 
+# Includes own annotations and augmentations to the code.
+
+library(tidyverse)
+library(tidymodels)
+library(nycflights13)    # for flight data
+library(skimr)           # for variable summaries
